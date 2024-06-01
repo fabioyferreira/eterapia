83930efb711e8ccf1d6bf41eaa9cacb5fe1f6154
Fabio Ferreira <fabioyferreira@gmail.com>
2024-06-01 12:31:36 -0300

    01-06-2026

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log(person.toUpperCase());
 console.log(person.length);
 
 
+const button = document.querySelector('button');
+
+button.onclick = function(){
+console.log('clicasse de novo');
+};
+button.addEventListener('de novo');
